145bc047d1b5eab166cb47a235fb853ff8d0a515
Hafsa600-dot <princehafsa156@gmail.com>
2024-10-29 18:25:49 +0500

    .

---
+++ variables-asg.js
@@ -0,0 +1,16 @@
+// Variables Asg
+// Chat GPT Asg
+let name = "Hafsa";
+let age = "15";
+let country = "Pakistan";
+
+console.log(name);
+console.log(age);
+console.log(country);
+
+let x = 5;
+let y = "9";
+let sum = x + y;
+console.log(sun);
+
+
