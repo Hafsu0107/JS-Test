4d3e6c82bfdaf2065a55b6af96c50ff6bae2ceb4
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-10 08:33:51 +0500

    .

--- function.js
+++ function.js
@@ -3,7 +3,35 @@
 
 // Chat gpt  Questtions
 
-function greetingCard(name) {
-  return name;
+function greet(name) {
+  return "Hello hafsa!";
 }
-console.log(greetingCard("Hello"));
+console.log(greet(name));
+
+//add numbers
+function addNumbers(num1, num2) {
+  return 8 + 9;
+}
+console.log(addNumbers(num1 + num2));
+
+// find max
+function findMax(a, b, c) {
+  return a, b, c; // return max
+}
+console.log(findMax(1, 2, 5));
+
+// find minimum
+function findMin(x, y, z) {
+  return x, y, z;
+}
+console.log(findMin(9, 10, 6));
+
+// Check Even or Odd
+
+function isEven(number) {
+  return number % 2 == 0;
+}
+console.log(isEven(5)); // output false bcz its odd
+console.log(isEven(4)); // output true bcz its even
+
+// Count Vowels in a string
