579595642f83c3c0c70e8e77f55dbd7b0fe582a3
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-14 01:05:33 +0500

    l

--- loops.js
+++ loops.js
@@ -1,7 +1,16 @@
 // for (let index = 0; index < array.length; index++) {
 //   const element = array[index];
 // }
-for (let index = 0; index < 10; index++) {
-  const element = index;
-  console.log(index);
+// for (let i = 0; i < 10; i++) {
+//   const element = i;
+//   console.log(i);
+// }
+// <=, <
+// loop in loop
+
+for (let j = 0; j <= 10; j++) {
+  console.log("Outer loop value: ${j}");
+  for (let a = 0; a <= 10; a++) {
+    console.log(a + "*" + j + " = " + a * j);
+  }
 }
