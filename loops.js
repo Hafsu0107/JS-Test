24ee09e82cc34151cda93e06d6efc393700e2147
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-15 01:28:43 +0500

    .

--- loops.js
+++ loops.js
@@ -14,3 +14,14 @@ for (let j = 0; j <= 10; j++) {
     console.log(a + "*" + j + " = " + a * j);
   }
 }
+
+for (let index = 1; index <= 10; index++) {
+  if (index == 8) {
+    console.log("Detected 8");
+    // break;
+    // continue;
+  }
+  console.log(index);
+}
+// While : mai hum condition check krty hai if else ki trha huti hai
+// tuu jo condition hugi usis hisab sai while loop chlega is mai hum var ko pehle likhty hai frist lienmai upper uske bdd phr us variable ko condiiton dete hai kai kia krna hai kia nh
