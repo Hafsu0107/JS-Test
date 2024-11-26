ee8497577074688a49720137e9341b205ad19c0b
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-26 15:20:38 +0500

    ,

--- oops.js
+++ oops.js
@@ -9,18 +9,48 @@
 // ## why use OOP
 
 // ## parts of OOP
-// Object literal 
+// Object literal
 
 // - Constructor function
 // - Prototypes
 // - Classes
 // - Instances (new, this)
 
-
 // ## 4 pillars
 // Abstraction -> Details hide krlenaa. eg: fetch.
-// Encapsulation ->wrrapap krlete hai jo jo chyee ussy acces krty hai  
+// Encapsulation ->wrrapap krlete hai jo jo chyee ussy acces krty hai
 // Inheritance
 // Polymorphism -> bht sary role play krna.
 
+const user = {
+  username: "hitesh",
+  loginCount: 8,
+  signedIn: true,
 
+  getUserDetails: function () {
+    //console.log("Got user details from database");
+    // console.log(`Username: ${this.username}`);
+    console.log(this);
+  },
+};
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+
+  this.greeting = function () {
+    console.log(`Welcome ${this.username}`);
+  };
+
+  return this;
+}
+
+const userOne = new User("hitesh", 12, true);
+const userTwo = new User("ChaiAurCode", 11, false);
+console.log(userOne.constructor);
+//console.log(userTwo);
