fa91d8db392306067ff198060ba9789c3b2333e3
Hafsa600-dot <princehafsa156@gmail.com>
2024-12-27 20:10:00 +0500

    .

---
+++ test.js
@@ -0,0 +1,31 @@
+let a = 0;
+console.log(a);
+
+// Create a variable with a boolean value and write a conditional statement based on its value.?
+let isStuding = true;
+if (isStuding) {
+  console.log("school is open");
+} else {
+  console.log("school is close");
+}
+var openShop = true;
+if (openShop) {
+  console.log("snacks are available");
+} else {
+  console.log("snacks are not available");
+}
+
+let isRunnig = false;
+if (isRunnig) {
+  console.log("race is start ");
+} else {
+  console.log("race is not start");
+}
+
+// Convert a string to a number using JavaScript.
+
+let str = "49";
+let num = Number(str);
+console.log(num);
+console.log(typeof num);
+
