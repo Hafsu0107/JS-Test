9f0324ac2392a7ad0bd435176836e718d5b10129
Hafsa600-dot <princehafsa156@gmail.com>
2024-12-29 05:55:04 +0500

    .

---
+++ promise-fetch/variables-.js
@@ -0,0 +1,14 @@
+//Variables
+// Declare three variables using var, let, and const and assign values to them.
+// Display their values in the console.
+
+let name = "hafsa";
+var city = "karachi";
+const country = "pakistan";
+
+console.log("name", name);
+console.log("city", city);
+console.log("country", country);
+
+// Create a variable using let and assign it a value.
+// Reassign the variable with a new value and display both values in the console.
