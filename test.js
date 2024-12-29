8cd717084c0fec29022b0c03404bd88c89acf8f7
Hafsa600-dot <princehafsa156@gmail.com>
2024-12-29 05:13:02 +0500

    .

--- test.js
+++ test.js
@@ -29,3 +29,41 @@ let num = Number(str);
 console.log(num);
 console.log(typeof num);
 
+// Write a program to concatenate two string variables.
+
+// Declare two string variables
+let firstName = "John";
+let lastName = "Doe";
+
+// Concatenate the strings
+let fullName = firstName + " " + lastName;
+
+// Output the result
+console.log("Full Name: " + fullName);
+
+
+// Write a program to create and display an object with at least three properties.
+
+// Create an object with three properties
+let person = {
+    name: "Alice",
+    age: 25,
+    occupation: "Web Developer"
+};
+
+// Display the object properties
+console.log("Name:", person.name);
+console.log("Age:", person.age);
+console.log("Occupation:", person.occupation);
+
+
+//02
+let school ={
+  name: "Educators",
+  branch: "Metrovill",
+  project: "Becomehouse"
+}
+console.log("Name;" , school.name);
+console.log("Branch;" , school.branch);
+console.log("Project;" , school.project);
+
